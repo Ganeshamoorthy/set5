32705d9258ec086957098a0269ec047ea5f2c479
Ganeshamoorthy <35378548+Ganeshamoorthy@users.noreply.github.com>
2018-03-13 16:07:24 +0530

    Update stract fun.c

--- stract fun.c
+++ stract fun.c
@@ -1,1 +1,27 @@
-
+#include<stdio.h>
+#include<conio.h>
+char*strcat(char*a,const char*b);
+void main()
+{
+   char a[20]="good";
+   char b[20]="morning";
+   clrscr();
+   puts(a);
+   puts(b);
+   strcat(a,b);
+   printf("after conatenation %s",a);
+   getch();
+}
+char*strcat(char*a,const char*b)
+{
+  int i=0,j=0;
+  while(a[i]!='\0')
+  i++;
+  while(b[j]!='\0');
+  {
+    a[i]=b[j];
+    i++;j++;
+  }
+  a[i]='\0';
+  return a;
+}
