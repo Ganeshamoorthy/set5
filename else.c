66df6dd5c4e94819ef5ebc007f8fa9500bc09dca
Ganeshamoorthy <35378548+Ganeshamoorthy@users.noreply.github.com>
2018-03-15 14:41:50 +0530

    Create else.c

---
+++ else.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("enter the value:\n");
+scanf("%d",&a,&b);
+if(a>b)
+{
+    printf(" value is high");
+    
+}
+else
+{
+   printf("value is low");
+}
+}
