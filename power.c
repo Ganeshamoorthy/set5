c1f43d8a9319ee87ce370597e9cd7217c5edddb3
Ganeshamoorthy <35378548+Ganeshamoorthy@users.noreply.github.com>
2018-03-16 14:56:30 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+	
+{
+	int a;
+	printf("enter the number:");
+	scanf("%d",&a);
+	
+		if(a%2==0)
+		{
+		printf("%d yes",a);
+		
+	}
+	
+	else 
+	{
+	printf("%d no",a);
+	}
+}
