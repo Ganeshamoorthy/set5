e6ce0a5f88506a7e2c033d2111b14bf6a8a917bc
Ganeshamoorthy <35378548+Ganeshamoorthy@users.noreply.github.com>
2018-03-20 15:57:27 +0530

    Create digit.c

---
+++ digit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) 
+{
+	int n,temp,a,b,c,d;
+	scanf("%d",&k);
+	temp=k;
+	if(temp!=2)
+	{
+		a=temp/100;
+		b=temp%100;
+		c=b/10;
+		d=b%10;
+	}
+	printf("%d %d %d",a,c,d);
+	
+	return 0;
+}
